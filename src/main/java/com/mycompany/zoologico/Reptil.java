9d3840d72d68461ee8af3d3a6fcf81cf0fb7f28e
Diego-Benavides <diego.benavides008@gmail.com>
2026-06-16 11:37:09 -0600

    Update Reptil.java

--- src/main/java/com/mycompany/zoologico/Reptil.java
+++ src/main/java/com/mycompany/zoologico/Reptil.java
@@ -25,6 +25,11 @@ public class Reptil extends Animal {
     }
 
     public void setTipoHabitat(String tipoHabitat) {
+
+        if (tipoHabitat == null) {
+           throw new IllegalArgumentException("El hábitat no puede estar vacío");
+        }
+
         this.tipoHabitat = tipoHabitat;
     }
 
@@ -33,7 +38,12 @@ public class Reptil extends Animal {
     }
 
     public void setTemperaturaRequerida(int temperaturaRequerida) {
-        this.temperaturaRequerida = temperaturaRequerida;
+
+    if (temperaturaRequerida < 0) {
+        throw new IllegalArgumentException("La temperatura no puede ser negativa");
+    }
+
+    this.temperaturaRequerida = temperaturaRequerida;
     }
 
     @Override
@@ -48,20 +58,4 @@ public class Reptil extends Animal {
     public String descripcion() {
         return super.descripcion() + " - Reptil ( " + tipoHabitat + temperaturaRequerida + "°) "; 
     }  
-    public void setTipoHabitat(String tipoHabitat) {
-
-    if (tipoHabitat == null) {
-        throw new IllegalArgumentException("El hábitat no puede estar vacío");
-    }
-
-    this.tipoHabitat = tipoHabitat;
-}
-    public void setTemperaturaRequerida(int temperaturaRequerida) {
-
-    if (temperaturaRequerida < 0) {
-        throw new IllegalArgumentException("La temperatura no puede ser negativa");
-    }
-
-    this.temperaturaRequerida = temperaturaRequerida;
-   }
 }
